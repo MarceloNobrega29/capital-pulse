47935809dbb0283952a65f14e96ca114a5e4a2c0
Marcelo Nóbrega <nobregamf29@hotmail.com>
2026-01-02 03:53:19 -0300

    feat: adiciona entity AccountStock

---
+++ src/main/java/capitalpulse/agregadordeinvestimentos/entity/AccountStock.java
@@ -0,0 +1,16 @@
+package capitalpulse.agregadordeinvestimentos.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "tb_account_stocks")
+public class AccountStock {
+
+}
