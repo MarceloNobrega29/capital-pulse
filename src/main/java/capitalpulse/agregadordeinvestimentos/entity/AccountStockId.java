59629e62d7a460f0d8d81a8b5050159929174652
Marcelo Nóbrega <nobregamf29@hotmail.com>
2026-01-02 03:55:31 -0300

    feat: adiciona entity AccountStockId

---
+++ src/main/java/capitalpulse/agregadordeinvestimentos/entity/AccountStockId.java
@@ -0,0 +1,17 @@
+package capitalpulse.agregadordeinvestimentos.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+import java.util.UUID;
+
+@Embeddable
+public class AccountStockId {
+
+    @Column(name = "account_id")
+    private UUID accountId;
+
+    @Column(name = "stock_id")
+    private String stockId;
+
+}
